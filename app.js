740e3723adfa6e9b2e67ec76bc5c49519d3cdd58
Mani Krishna <151340390+manikrishna5@users.noreply.github.com>
2024-10-01 18:17:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const max =prompt("Enter the max number");
+console.log(max);
+
+const random = Math.floor(Math.random()*max)+1;
+let guess = prompt("Guess the number");
+while(true){
+    if(guess == "quit"){
+        console.log("User quit");
+        break;
+    }
+    if(guess == random){
+        console.log("Congrats!! You guessed it right, random number was"+ random);
+        break;
+    } else{
+        guess = prompt("Your guess was wrong. Please try again");
+    }
+}
